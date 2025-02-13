7854503b8a5c1fcd4a6c6d7b867c86f4e9f4e31a
Belmiro Matos Da Costa <bmatos-d@c2r6s4.42madrid.com>
2025-02-14 00:49:17 +0100

    migration

--- Inception/srcs/requirements/nginx/nginx.sh
+++ Inception/srcs/requirements/nginx/nginx.sh
@@ -1,5 +1,27 @@
 #! bin/sh
 
+#┌─┐
+#│ │
+#├─┤
+#│ │	
+#└─┘
+
+#┌─────────────────────────────────────────────────────────────────────────┐
+#│               GENERATING AND STORING THE CERTIFICATE                    │	
+#├─────────────────────────────────────────────────────────────────────────┤
+#│ RSA keys are what everyone uses so to understand what I'm doing and     │
+#│ avoid copy and pasting im using an ECC key.                             │
+#└─────────────────────────────────────────────────────────────────────────┘
+
+
+
+openssl genpkey -algorithm EC -pkeyopt ec_paramgen_curve:prime256v1 -out /etc/ssl/private/ecc_private.key
+
+#openssl ecparam -genkey -name prime256v1 -out /etc/ssl/private/ecc_private.key
+
+#openssl ec -in /etc/ssl/private/ecc_private.key -check -noout                          # checks if the ecc key is valid
+
+#todo why is certs hidden?
 #req 
 # This command primarily creates and processes certificate requests (CSRs) 
 # in PKCS#10 format. It can additionally create self-signed certificates 
@@ -29,9 +51,9 @@
 
 #-days duration
 # How long the request lasts
-
-openssl ecparam -genkey -name prime256v1 -out /etc/ssl/private/ecc_private.key
 openssl req -new -x509 -days 365 -key /etc/ssl/private/ecc_private.key -out  /etc/ssl/certs/ecc_cert.crt  -subj "/C=ES/L=MD/O=42/OU=student/CN=bmatos-d.42.ma"
-openssl ec -in /etc/ssl/private/ecc_private.key -check -noout
+
+
+
 
 nginx -g "daemon off;"
