c4b6a6a18c25053df2e9689ca544bf42bd412516
Belmiro Matos Da Costa <bmatos-d@c1r12s1.42madrid.com>
2025-02-20 23:11:59 +0100

    a

--- Inception/srcs/requirements/nginx/nginx.sh
+++ Inception/srcs/requirements/nginx/nginx.sh
@@ -139,7 +139,7 @@ http
             index                   index.php;
             location ~ \.php$
             {
-                try_files                       \$uri =404;
+                try_files                       \$uri =BITCH;
                 include                         fastcgi_params;
             }
         }
