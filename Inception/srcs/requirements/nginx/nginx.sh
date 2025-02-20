5a77e814221b7cc80572cbeff209a79ba2d78bee
Belmiro Matos Da Costa <bmatos-d@c1r12s1.42madrid.com>
2025-02-20 23:08:26 +0100

    a

--- Inception/srcs/requirements/nginx/nginx.sh
+++ Inception/srcs/requirements/nginx/nginx.sh
@@ -147,6 +147,10 @@ http
 }
 
 "                             >> /etc/nginx/nginx.conf
+cat /etc/hosts
+
+echo "
+127.0.0.1 www.bmatos-d.42.ma bmatos-d.42.ma" >> /etc/hosts
                 #fastcgi_pass                    wordpress:9000;
                 #fastcgi_param SCRIPT_FILENAME   $document_root$fastcgi_script_name;
 
