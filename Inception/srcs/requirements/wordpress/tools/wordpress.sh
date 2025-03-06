f748ef68aea7a892e79a0d79c1a3e2650364de49
Belmiro Matos Da Costa <bmatos-d@c1r6s6.42madrid.com>
2025-03-06 14:07:38 +0100

    a

--- Inception/srcs/requirements/wordpress/tools/wordpress.sh
+++ Inception/srcs/requirements/wordpress/tools/wordpress.sh
@@ -53,4 +53,4 @@ sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm
 mkdir /run/php
 #wp plugin install redis-cache --activate --allow-root
 wp redis enable --allow-root
-/usr/sbin/php-fpm7.3 -F
+/usr/sbin/php-fpm -F
