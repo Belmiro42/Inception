9168d1d29472d58652f9d888e2adfeb65c3f9006
Belmiro Matos Da Costa <bmatos-d@c1r6s6.42madrid.com>
2025-03-06 13:55:24 +0100

    a

--- Inception/srcs/requirements/wordpress/tools/wordpress.sh
+++ Inception/srcs/requirements/wordpress/tools/wordpress.sh
@@ -30,7 +30,7 @@ rm      *               -rf
 # This command downloads wordpress CLI fromt the official repo
 
 curl    -O                                  https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-cmhod   +x                                  wp-cli.phar
+chmod   +x                                  wp-cli.phar
 mv      wp-cli.phar                         /usr/local/bin/wp
 wp      core download                       --allow-root
 mv      /var/www/html/wp-config-sample.php  /var/www/html/wp-config.php
