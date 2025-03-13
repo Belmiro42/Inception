99c56cf48ac75d7c487daebd4ebecdc098af1ea6
Belmiro Matos Da Costa <bmatos-d@c1r4s2.42madrid.com>
2025-03-13 16:36:41 +0100

    a

--- Inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 apk add --no-cache mariadb mariadb-client openrc
-
+rc-service mariadb start
 sleep 5;
 
-mysql -u -p << EOF
+mysql -u root -e << EOF
 CREATE DATABASE IF NOT EXISTS $db1_name ;
 CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;
 GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;
