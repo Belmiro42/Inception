9c234682d11dd2ec5581e2e1790eaff488613129
Belmiro Matos Da Costa <bmatos-d@c1r4s2.42madrid.com>
2025-03-13 16:51:49 +0100

    a

--- Inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-apk add --no-cache mariadb mariadb-client openrc
-rc-service mariadb start
-sleep 5;
 
-mysql -u root -e << EOF
+apk add  mariadb mariadb-client openrc
+echo "apk add  mariadb mariadb-client openrc"
+
+rc-service mariadb start
+echo "rc-service mariadb start"
+
+sleep 5;
+echo "sleep 5;"
+
+
+echo ""
+
+mysql -u  << EOF
 CREATE DATABASE IF NOT EXISTS $db1_name ;
 CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;
 GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;
@@ -13,5 +22,10 @@ FLUSH PRIVILEGES;
 EOF
 
 rc-update add mariadb default
+echo "rc-update add mariadb default"
+
 mysqld --initialize-insecure --datadir=/var/lib/mysql
+echo "mysqld --initialize-insecure --datadir=/var/lib/mysql"
+
 rc-service mariadb restart
+echo "rc-service mariadb restart"
