2742df216c34d4121e4ab64a44aae8d82ab2ef26
Belmiro Matos Da Costa <bmatos-d@c1r4s2.42madrid.com>
2025-03-13 18:53:18 +0100

    a

--- Inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -6,19 +6,22 @@ apk add --no-cache mariadb mariadb-client openrc
 
 mkdir -p /var/lib/mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
 
+netstat -ln | grep mysqld
 
 echo "mysqld --initialize-insecure --datadir=/var/lib/mysql"
-mysqld --user=mysql --datadir=/var/lib/mysql/mysql &
+mysqld --user=mysql --datadir=/var/lib/mysql &
 
-sleep 5;
 
-echo "mysql -u root -p << EOF"
-
-mysql -u root << EOF
-CREATE DATABASE IF NOT EXISTS $db1_name ;
-CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;
-GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;
-FLUSH PRIVILEGES;
-EOF
+#sleep 5;
+#
+#echo "mysql -u root -p << EOF"
+#
+#mysql -u root << EOF
+#CREATE DATABASE IF NOT EXISTS $db1_name ;
+#CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;
+#GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;
+#ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;
+#FLUSH PRIVILEGES;
+#EOF
