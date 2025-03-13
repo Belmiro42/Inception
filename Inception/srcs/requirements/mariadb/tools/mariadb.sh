1b625a49e6b87bc77988e0d0aa586ed97878027b
Belmiro Matos Da Costa <bmatos-d@c1r4s2.42madrid.com>
2025-03-13 18:31:43 +0100

    a

--- Inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -3,14 +3,19 @@
 echo "apk add --no-cache mariadb mariadb-client openrc"
 apk add --no-cache mariadb mariadb-client openrc
 
+
+mkdir -p /var/lib/mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
+
+
 echo "mysqld --initialize-insecure --datadir=/var/lib/mysql"
-mysqld --user=mysql --datadir=/var/lib/mysql
+mysqld --user=mysql --datadir=/var/lib/mysql &
 
 sleep 5;
 
 echo "mysql -u root -p << EOF"
 
-mysql --user=mysql -p << EOF
+mysql -u root << EOF
 CREATE DATABASE IF NOT EXISTS $db1_name ;
 CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;
 GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;
