c1122309cd1fa688c43c359f0660846fe991ae26
Belmiro Matos Da Costa <bmatos-d@c1r4s2.42madrid.com>
2025-03-13 18:26:37 +0100

    a

--- Inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -10,7 +10,7 @@ sleep 5;
 
 echo "mysql -u root -p << EOF"
 
-mysql --user=mysq -p << EOF
+mysql --user=mysql -p << EOF
 CREATE DATABASE IF NOT EXISTS $db1_name ;
 CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;
 GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;
