6f05968275b458c8cce298751024fd5d6da24e37
Belmiro Matos Da Costa <bmatos-d@c1r4s2.42madrid.com>
2025-03-13 17:09:25 +0100

    a

--- Inception/srcs/requirements/mariadb/tools/mariadb.sh
+++ Inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -12,6 +12,9 @@ sleep 5;
 
 echo "MYSQL"
 
+echo "rc-update add mariadb default"
+rc-update add mariadb default
+
 mysql << EOF
 CREATE DATABASE IF NOT EXISTS $db1_name ;
 CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;
@@ -19,9 +22,6 @@ GRANT ALL PRIVILEGES ON $db1_name.* TO '$db1_user'@'%' ;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;
 FLUSH PRIVILEGES;
 EOF
-
-echo "rc-update add mariadb default"
-rc-update add mariadb default
 
 echo "rc-service mariadb restart"
 rc-service mariadb restart
